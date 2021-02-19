bfcccdc7bdff4c0bc6c9a64d4b26f47284a527c2
Jhonatant98 <73711413+Jhonatant98@users.noreply.github.com>
2021-02-18 22:39:49 -0500

    Punto 18

--- src/objetos/PersonaService.java
+++ src/objetos/PersonaService.java
@@ -1,7 +1,6 @@
-package Objetos;
+package objetos;
 
 import java.util.ArrayList;
-import java.util.List;
 import java.util.Scanner;
 
 public class PersonaService implements IPersonas{
--- src/principal/PuntosTaller.java
+++ src/principal/PuntosTaller.java
@@ -1,4 +1,4 @@
-package Principal;
+package principal;
 
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
